4a73b9ae57ec884cc0559f28b1f3512035ce2c50
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-12 17:03:44 +0100

    imported seedColors and Palette and render the Palette component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import Palette from './Palette'
+import seedColors from './seedColors'
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Palette {...seedColors[4]} />
+    </div>
   );
 }
 
