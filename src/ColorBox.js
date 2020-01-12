8a29221b4411cc62af50ad7d11d3441efe4bff51
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-12 17:11:08 +0100

    print out the color name

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,6 +4,7 @@ export class ColorBox extends Component {
   render() {
     return (
       <div style={{ background: this.props.background }} className='ColorBox'>
+        <span>{this.props.name}</span>
         <span>MORE</span>
       </div>
     )
