3c7d991e7ad82ab6442ccb462e229a3cad5bd8a5
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-13 19:06:38 +0100

    wrapped the SLider in container

--- src/Palette.js
+++ src/Palette.js
@@ -25,14 +25,16 @@ export class Palette extends Component {
 
     return (
       <div className='Palette'>
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel}
-        />
-        <div className="Palette-colors">
+        <div className='slider'>
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={this.changeLevel}
+          />
+        </div>
+        <div className='Palette-colors'>
           {colorBoxes}
         </div>
       </div>
