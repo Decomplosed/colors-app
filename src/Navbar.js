6d026bae743fdf39a66024df9e19241b92de53c7
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-13 19:36:10 +0100

    moved the Slider code to the Navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
 
 export class Navbar extends Component {
   render() {
@@ -6,6 +8,15 @@ export class Navbar extends Component {
       <header className='Navbar'>
         <div className="logo">
           <a href='#'>reactcolorpicker</a>
+        </div>
+        <div className='slider'>
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={this.changeLevel}
+          />
         </div>
       </header>
     )
--- src/Palette.js
+++ src/Palette.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react'
 import ColorBox from './ColorBox'
-import Slider from 'rc-slider'
-import 'rc-slider/assets/index.css'
 import './Palette.css'
 
 
@@ -26,15 +24,6 @@ export class Palette extends Component {
 
     return (
       <div className='Palette'>
-        <div className='slider'>
-          <Slider
-            defaultValue={level}
-            min={100}
-            max={900}
-            step={100}
-            onAfterChange={this.changeLevel}
-          />
-        </div>
         <div className='Palette-colors'>
           {colorBoxes}
         </div>
