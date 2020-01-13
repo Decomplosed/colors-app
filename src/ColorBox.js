e128be864f9a13279938c4933ffd96c2f963f181
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-13 16:14:01 +0100

    fixed the the nested container to be out of the copy-overlay

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -21,11 +21,10 @@ export class ColorBox extends Component {
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState} >
         <div style={{ background }} className='ColorBox'>
-          <div style={{ background }} className={`copy-overlay ${copied && 'show'}`}>
-            <div className={`copy-msg ${copied && 'show'}`}>
-              <h1>copied!</h1>
-              <p>{this.props.background}</p>
-            </div>
+          <div style={{ background }} className={`copy-overlay ${copied && 'show'}`} />
+          <div className={`copy-msg ${copied && 'show'}`}>
+            <h1>copied!</h1>
+            <p>{this.props.background}</p>
           </div>
           <div className="copy-container">
             <div className="box-content">
