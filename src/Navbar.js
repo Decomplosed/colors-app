1e8d0130efa78af4a17ba1fc11ac47938250c02d
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-14 10:05:14 +0100

    added onChange attribute to the Select component

--- src/Navbar.js
+++ src/Navbar.js
@@ -26,7 +26,7 @@ export class Navbar extends Component {
           </div>
         </div>
         <div className="select-container">
-          <Select>
+          <Select onChange={this.props.handleChange}>
             <MenuItem value='hex'>HEX - #ffffff</MenuItem>
             <MenuItem value='rgb'>RGB - rgb(255,255,255)</MenuItem>
             <MenuItem value='rgba'>RGBA - rgba(255,255,255, 1.0)</MenuItem>
