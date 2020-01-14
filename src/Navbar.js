7e46d0fbdbf70939b09566b5db188efca56f5328
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-14 10:22:04 +0100

    changed href value to avoid warnings inside console

--- src/Navbar.js
+++ src/Navbar.js
@@ -23,7 +23,7 @@ export class Navbar extends Component {
     return (
       <header className='Navbar'>
         <div className="logo">
-          <a href='#'>reactcolorpicker</a>
+          <a href='/'>reactcolorpicker</a>
         </div>
         <div className="slider-container">
           <span>Level: {level}</span>
