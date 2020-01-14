6eeef77ae9cb1354a6104c4efbbef9809af30ee9
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-14 10:03:52 +0100

    created three hardcoded MenuItems inside Select

--- src/Navbar.js
+++ src/Navbar.js
@@ -27,7 +27,9 @@ export class Navbar extends Component {
         </div>
         <div className="select-container">
           <Select>
-
+            <MenuItem value='hex'>HEX - #ffffff</MenuItem>
+            <MenuItem value='rgb'>RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value='rgba'>RGBA - rgba(255,255,255, 1.0)</MenuItem>
           </Select>
         </div>
       </header>
