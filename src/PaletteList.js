e2f65aca4f5078fc039e1b0dae3fba3378204e63
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-15 17:17:47 +0100

    destructured the palette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 export class PaletteList extends Component {
   render() {
+    const { palettes } = this.props
     return (
       <div>
         <h1>Palette Colors</h1>
