75eaaeb9c9b7fb59ea9a7c9c12494fc71627cff1
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-15 17:11:31 +0100

    generatePalette of found palette

--- src/App.js
+++ src/App.js
@@ -19,8 +19,13 @@ class App extends Component {
           exact
           path='/palette/:id'
           render={(routeProps) => (
-            <Palette palette={this.findPalette(routeProps.match.params.id)} />
-          )} />
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
       </Switch>
       // <div className="App" >
       //   <Palette palette={generatePalette(seedColors[4])} />
