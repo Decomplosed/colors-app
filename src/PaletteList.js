d1f030c0b960785724648c3805ff2c9ed0e172fb
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-15 17:18:56 +0100

    render all paletteNames

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -5,7 +5,10 @@ export class PaletteList extends Component {
     const { palettes } = this.props
     return (
       <div>
-        <h1>Palette Colors</h1>
+        <h1>React Colors</h1>
+        {palettes.map(palette => (
+          <h1>{palette.paletteName}</h1>
+        ))}
       </div>
     )
   }
