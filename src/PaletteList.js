e1044ff32e50b8514eb0b22e256996d86addf4e1
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-15 17:14:19 +0100

    started creating a PaletteList component

---
+++ src/PaletteList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class PaletteList extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default PaletteList
