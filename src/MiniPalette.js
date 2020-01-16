f012b79151750c4809a2f210bdd4a72a35d191a9
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-16 18:49:18 +0100

    removed styles defined before

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -2,13 +2,7 @@ import React from 'react'
 import  {withStyles} from '@material-ui/styles'
 
 const styles = {
-  main: {
-    backgroundColor: 'purple',
-    border: '3px solid teal'
-  },
-  secondary: {
-    backgroundColor: 'pink'
-  }
+
 }
 
 function MiniPalette(props) {
