0b4933d1b36cbf635489e9f363fb6ee593584c14
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-16 22:15:34 +0100

    added styles to the root and gave classNames to the elements of the layout

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react'
 import MiniPalette from './MiniPalette'
 import  {withStyles} from '@material-ui/styles'
 
+const styles = {
+  root: {
+    backgroundColor: 'blue',
+
+  }
+}
+
 export class PaletteList extends Component {
   render() {
     const { palettes } = this.props
     return (
-      <div>
+      <div className={this.props.classes.root}>
+        <div className='container'></div>
         <h1>React Colors</h1>
         {palettes.map(palette => (
           <MiniPalette {...palette}/>
