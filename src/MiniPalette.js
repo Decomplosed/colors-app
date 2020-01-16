8b68f5b5856ff939fb9f225b55e428e58d87f7aa
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-16 18:54:49 +0100

    added styles to the title

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -4,6 +4,7 @@ import  {withStyles} from '@material-ui/styles'
 const styles = {
   root: {
     backgroundColor: 'white',
+    border: '1px solid black',
     borderRadius: '5px',
     padding: '0.5rem',
     position: 'relative',
@@ -16,7 +17,14 @@ const styles = {
     backgroundColor: 'grey'
   },
   title: {
-
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0',
+    color: 'black',
+    paddingTop: '0.5rem',
+    fontSize: '1rem',
+    position: 'relative'
   },
   emoji: {
 
@@ -31,7 +39,7 @@ function MiniPalette(props) {
       <div className={classes.colors}>
 
       </div>
-<h5 className={classes.title}>{paletteName} <span className={classes.emoji}>{emoji}</span></h5>
+      <h5 className={classes.title}>{paletteName} <span className={classes.emoji}>{emoji}</span></h5>
     </div>
   )
 }
