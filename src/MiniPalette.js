3b03f29d6b86e71a8710f7c88b7ae7b1f8fad9b1
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-16 16:52:12 +0100

    import iwthStyles

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import  {withStyles} from '@material-ui/styles'
 
-function MiniPalette() {
+function MiniPalette(props) {
   return (
     <div>
-      
+      <h1>Mini Palette</h1>
     </div>
   )
 }
