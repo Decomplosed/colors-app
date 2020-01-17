bda6f2dbc29609a77944c57747601e1766f91cb6
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-17 12:11:34 +0100

    stop propagation onClick of the Link

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,24 +1,24 @@
-import React, { Component } from 'react';
-import { CopyToClipboard } from 'react-copy-to-clipboard';
-import { Link } from 'react-router-dom';
-import './ColorBox.css';
+import React, { Component } from 'react'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import { Link } from 'react-router-dom'
+import './ColorBox.css'
 
 export class ColorBox extends Component {
 	constructor(props) {
-		super(props);
-		this.state = { copied: false };
-		this.changeCopyState = this.changeCopyState.bind(this);
+		super(props)
+		this.state = { copied: false }
+		this.changeCopyState = this.changeCopyState.bind(this)
 	}
 
 	changeCopyState() {
 		this.setState({ copied: true }, () => {
-			setTimeout(() => this.setState({ copied: false }), 1500);
-		});
+			setTimeout(() => this.setState({ copied: false }), 1500)
+		})
 	}
 
 	render() {
-		const { name, background } = this.props;
-		const { copied } = this.state;
+		const { name, background } = this.props
+		const { copied } = this.state
 		return (
 			<CopyToClipboard text={background} onCopy={this.changeCopyState}>
 				<div style={{ background }} className='ColorBox'>
@@ -36,13 +36,13 @@ export class ColorBox extends Component {
 						</div>
 						<button className='copy-button'>Copy</button>
 					</div>
-					<Link to='/'>
+					<Link to='/' onClick={e => e.stopPropagation()}>
 						<span className='see-more'>More</span>
 					</Link>
 				</div>
 			</CopyToClipboard>
-		);
+		)
 	}
 }
 
-export default ColorBox;
+export default ColorBox
