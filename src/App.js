04687770fa8efdd587753b33cce86744deabc43b
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-17 12:36:32 +0100

    formatted with not using tabs

--- src/App.js
+++ src/App.js
@@ -8,43 +8,43 @@ import seedColors from './seedColors'
 import { generatePalette } from './colorHelpers'
 
 class App extends Component {
-	findPalette(id) {
-		return seedColors.find(palette => palette.id === id)
-	}
+  findPalette(id) {
+    return seedColors.find(palette => palette.id === id)
+  }
 
-	render() {
-		return (
-			<Switch>
-				<Route
-					exact
-					path='/'
-					render={routeProps => (
-						<PaletteList palettes={seedColors} {...routeProps} />
-					)}
-				/>
-				<Route
-					exact
-					path='/palette/:id'
-					render={routeProps => (
-						<Palette
-							palette={generatePalette(
-								this.findPalette(routeProps.match.params.id)
-							)}
-						/>
-					)}
-				/>
-				<Route
-					exact
-					path='/palette/:paletteId/:colorId'
-					render={() => <SingleColorPalette />}
-				/>
-			</Switch>
+  render() {
+    return (
+      <Switch>
+        <Route
+          exact
+          path='/'
+          render={routeProps => (
+            <PaletteList palettes={seedColors} {...routeProps} />
+          )}
+        />
+        <Route
+          exact
+          path='/palette/:id'
+          render={routeProps => (
+            <Palette
+              palette={generatePalette(
+                this.findPalette(routeProps.match.params.id)
+              )}
+            />
+          )}
+        />
+        <Route
+          exact
+          path='/palette/:paletteId/:colorId'
+          render={() => <SingleColorPalette />}
+        />
+      </Switch>
 
-			// <div className="App" >
-			//   <Palette palette={generatePalette(seedColors[4])} />
-			// </div>
-		)
-	}
+      // <div className="App" >
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
+    )
+  }
 }
 
 export default App
