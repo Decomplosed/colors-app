545503a6c76ff4d2bae3700d06df16b0e20dc2ce
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-17 12:45:36 +0100

    extracted the paletteId and color id from props

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -17,7 +17,7 @@ export class ColorBox extends Component {
   }
 
   render() {
-    const { name, background } = this.props
+    const { name, background, paletteId, id } = this.props
     const { copied } = this.state
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
