13001de5ace10d9b04ffcab6d708ea5a3061f24b
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-17 12:35:40 +0100

    import SingleColorPalette and render under last Route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from 'react-router-dom'
 import './App.css'
 import Palette from './Palette'
 import PaletteList from './PaletteList'
+import SingleColorPalette from './SingleColorPalette'
 import seedColors from './seedColors'
 import { generatePalette } from './colorHelpers'
 
@@ -35,7 +36,7 @@ class App extends Component {
 				<Route
 					exact
 					path='/palette/:paletteId/:colorId'
-					render={() => <h1>SINGLE COLOR PAGE!</h1>}
+					render={() => <SingleColorPalette />}
 				/>
 			</Switch>
 
