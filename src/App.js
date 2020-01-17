87d6d40859d0502ae9429be14cd34b13dca61d51
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-17 12:20:41 +0100

    formatted App.js file

--- src/App.js
+++ src/App.js
@@ -1,49 +1,49 @@
-import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom';
-import './App.css';
-import Palette from './Palette';
-import PaletteList from './PaletteList';
-import seedColors from './seedColors';
-import { generatePalette } from './colorHelpers';
+import React, { Component } from 'react'
+import { Route, Switch } from 'react-router-dom'
+import './App.css'
+import Palette from './Palette'
+import PaletteList from './PaletteList'
+import seedColors from './seedColors'
+import { generatePalette } from './colorHelpers'
 
 class App extends Component {
-  findPalette(id) {
-    return seedColors.find(palette => palette.id === id);
-  }
+	findPalette(id) {
+		return seedColors.find(palette => palette.id === id)
+	}
 
-  render() {
-    return (
-      <Switch>
-        <Route
-          exact
-          path='/'
-          render={routeProps => (
-            <PaletteList palettes={seedColors} {...routeProps} />
-          )}
-        />
-        <Route
-          exact
-          path='/palette/:id'
-          render={routeProps => (
-            <Palette
-              palette={generatePalette(
-                this.findPalette(routeProps.match.params.id)
-              )}
-            />
-          )}
-        />
-        <Route
-          exact
-          path='/palette/:paletteId/:colorId'
-          render={() => <h1>SINGLE COLOR PAGE!</h1>}
-        />
-      </Switch>
+	render() {
+		return (
+			<Switch>
+				<Route
+					exact
+					path='/'
+					render={routeProps => (
+						<PaletteList palettes={seedColors} {...routeProps} />
+					)}
+				/>
+				<Route
+					exact
+					path='/palette/:id'
+					render={routeProps => (
+						<Palette
+							palette={generatePalette(
+								this.findPalette(routeProps.match.params.id)
+							)}
+						/>
+					)}
+				/>
+				<Route
+					exact
+					path='/palette/:paletteId/:colorId'
+					render={() => <h1>SINGLE COLOR PAGE!</h1>}
+				/>
+			</Switch>
 
-      // <div className="App" >
-      //   <Palette palette={generatePalette(seedColors[4])} />
-      // </div>
-    );
-  }
+			// <div className="App" >
+			//   <Palette palette={generatePalette(seedColors[4])} />
+			// </div>
+		)
+	}
 }
 
-export default App;
+export default App
