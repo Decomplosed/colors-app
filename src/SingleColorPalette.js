f753f1c217c298f80e8276958d90d0530ee631c9
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-18 15:33:35 +0100

    make the ColorBox to hbe 50% height inside SingleColorPalette

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -41,7 +41,7 @@ export class SingleColorPalette extends Component {
     ))
 
     return (
-      <div className='Palette'>
+      <div className='SingleColorPalette Palette'>
         <Navbar handleChange={this.changeFormat} showingAllColors={false}/>
         <div className='Palette-colors'>{colorBoxes}</div>
         <PaletteFooter paletteName={paletteName} emoji={emoji}/>
