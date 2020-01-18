64a6120ba6bc6cb78169110a41d7716b94ea6c9a
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-18 16:07:31 +0100

    same for copy button inside ColorBox container

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -37,7 +37,7 @@ export class ColorBox extends Component {
             <div className='box-content'>
               <span className={isDarkColor && 'light-text'}>{name}</span>
             </div>
-            <button className='copy-button'>Copy</button>
+            <button className={`copy-button ${isLightColor && 'dark-text'}`}>Copy</button>
           </div>
           {showLink && (
             <Link to={moreUrl} onClick={e => e.stopPropagation()}>
