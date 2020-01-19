0ad7144569fb6189ff8de39aabba0efede20c4f6
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-19 21:23:21 +0100

    move copy button styles to the styles object

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -30,6 +30,25 @@ const styles = {
     textAlign: 'center',
     lineHeight: '30px',
     textTransform: 'uppercase'
+  },
+  copyBtn: {
+    width: '100px',
+    height: ' 30px',
+    position: 'absolute',
+    display: 'inline-block',
+    top: '50%',
+    left: '50%',
+    marginLeft: '-50px',
+    marginTop: '-15px',
+    textAlign: 'center',
+    outline: 'none',
+    background: 'rgba(255, 255, 255, 0.3)',
+    fontSize: '1rem',
+    lineHeight: '30px',
+    color: '#fff',
+    textTransform: 'uppercase',
+    textDecoration: 'none',
+    border: ' none'
   }
 }
 
