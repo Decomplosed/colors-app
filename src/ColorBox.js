2dc7b9e74928273ccfaaa3d3ade19ce6102ea6be
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-19 22:26:10 +0100

    implemented boxContent styles

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -113,7 +113,7 @@ export class ColorBox extends Component {
             <p className={classes.copyText}>{this.props.background}</p>
           </div>
           <div className="copy-container">
-            <div className="box-content">
+            <div className={classes.boxContent}>
               <span className={classes.colorName}>{name}</span>
             </div>
             <button className={classes.copyBtn}>Copy</button>
