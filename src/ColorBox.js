189d2b54e173e1ef1c4130d5221b761d22e0bd66
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-19 22:28:23 +0100

    added transition to the styles inside react

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -16,7 +16,8 @@ const styles = {
     cursor: 'pointer',
     marginBottom: '-3.5px',
     '&:hover button': {
-      opacity: '1'
+      opacity: '1',
+      transition: '0.5s'
     }
   },
   copyText: {
