2cb2591f58f8a0285385dc209e782fdeb3d55cbf
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-20 16:18:39 +0100

    import with styles

--- src/Palette.js
+++ src/Palette.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import ColorBox from './ColorBox'
 import Navbar from './Navbar'
 import PaletteFooter from './PaletteFooter'
+import { withStyles } from '@material-ui/styles'
+
 import './Palette.css'
 
 export class Palette extends Component {
