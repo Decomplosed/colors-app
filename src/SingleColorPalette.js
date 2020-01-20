aeed580410336dde2ad10963dcc45ad4b1348da6
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-20 17:03:38 +0100

    moved the anchor styles inside go back button

--- src/SingleColorPalette.js
+++ src/SingleColorPalette.js
@@ -25,7 +25,23 @@ const styles = {
     opacity: '1',
     backgroundColor: 'black',
     '& a': {
-      
+      color: 'white',
+      width: '100px',
+      height: ' 30px',
+      position: 'absolute',
+      display: 'inline-block',
+      top: '50%',
+      left: '50%',
+      marginLeft: '-50px',
+      marginTop: '-15px',
+      textAlign: 'center',
+      outline: 'none',
+      background: 'rgba(255, 255, 255, 0.3)',
+      fontSize: '1rem',
+      lineHeight: '30px',
+      textTransform: 'uppercase',
+      textDecoration: 'none',
+      border: 'none'
     }
   }
 }
