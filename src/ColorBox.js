7538df92c79b05010e4ce2a94f01ff3319e27b22
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-20 20:58:41 +0100

    moved styles from COmponent file to the PaletteStyles.js

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'react'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { Link } from 'react-router-dom'
-import { withStyles } from '@material-ui/styles'
 import styles from './styles/ColorBoxStyles'
-
-import './ColorBox.css'
+import { withStyles } from '@material-ui/styles'
 
 class ColorBox extends Component {
   constructor(props) {
