0a9533f3c56910cc775bbf742afe629dc3c46cfe
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-21 22:43:40 +0100

    moved Navbar styles to separated file

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,54 +10,6 @@ import { withStyles } from '@material-ui/styles'
 import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
 
-const styles = {
-  Navbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-start',
-    height: '6vh'
-  },
-  logo: {
-    marginRight: '15px',
-    padding: '0 13px',
-    fontSize: '22px',
-    backgroundColor: '#eceff1',
-    fontFamily: 'Roboto',
-    height: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    '& a': {
-      textDecoration: 'none',
-      color: '#000000'
-    }
-  },
-  slider: {
-    width: '340px',
-    margin: '0 10px',
-    display: 'inline-block',
-    '& .rc-slider-rail': {
-      height: '8px'
-    },
-    '& .rc-slider-handle, .rc-slider-handle:active, .rc-slider-handle:focus, .rc-slider-handle:hover': {
-      backgroundColor: 'green',
-      outline: 'none',
-      border: '2px solid green',
-      boxShadow: 'none',
-      width: '13px',
-      height: '13px',
-      marginLeft: '-7px',
-      marginTop: '-3px'
-    },
-    '& .rc-slider-track': {
-      backgroundColor: 'transparent'
-    }
-  },
-  selectContainer: {
-    marginLeft: 'auto',
-    marginRight: '1rem'
-  }
-}
-
 export class Navbar extends Component {
   constructor(props) {
     super(props)
