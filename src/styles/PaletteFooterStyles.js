2ba04c1143baeba962847bdbbd7fd465627c92f6
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-21 22:03:26 +0100

    moved PaletteFooter styles to the separated file

---
+++ src/styles/PaletteFooterStyles.js
@@ -0,0 +1,14 @@
+export default {
+  PaletteFooter: {
+    backgroundColor: '#fff',
+    height: '5vh',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    fontWeight: 'bold'
+  },
+  Emoji: {
+    fontSize: '1.5rem',
+    margin: '1rem'
+  }
+}
