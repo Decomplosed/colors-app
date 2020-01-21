28593dd159758c3d3e8c5ef1de808fee7dc26734
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-21 22:30:01 +0100

    finished formatting the styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -15,7 +15,7 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'flex-start',
-    height: '6vh',
+    height: '6vh'
   },
   logo: {
     marginRight: '15px',
@@ -26,10 +26,10 @@ const styles = {
     height: '100%',
     display: 'flex',
     alignItems: 'center',
-  },
-  logo a: {
-    textDecoration: 'none',
-    color: '#000000'
+    '& a': {
+      textDecoration: 'none',
+      color: '#000000'
+    }
   },
   slider: {
     width: '340px',
@@ -59,42 +59,51 @@ export class Navbar extends Component {
     const { level, changeLevel, showingAllColors } = this.props
     const { format } = this.state
     return (
-      <header className='Navbar'>
+      <header className="Navbar">
         <div className="logo">
-          <Link to='/'>reactcolorpicker</Link>
+          <Link to="/">reactcolorpicker</Link>
         </div>
-        { showingAllColors && (
-        <div className="slider-container">
-          <span>Level: {level}</span>
-          <div className='slider'>
-            <Slider
-              defaultValue={level}
-              min={100}
-              max={900}
-              step={100}
-              onAfterChange={changeLevel}
-            />
+        {showingAllColors && (
+          <div className="slider-container">
+            <span>Level: {level}</span>
+            <div className="slider">
+              <Slider
+                defaultValue={level}
+                min={100}
+                max={900}
+                step={100}
+                onAfterChange={changeLevel}
+              />
+            </div>
           </div>
-        </div>
         )}
         <div className="select-container">
           <Select value={format} onChange={this.handleFormatChange}>
-            <MenuItem value='hex'>HEX - #ffffff</MenuItem>
-            <MenuItem value='rgb'>RGB - rgb(255,255,255)</MenuItem>
-            <MenuItem value='rgba'>RGBA - rgba(255,255,255, 1.0)</MenuItem>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255, 1.0)</MenuItem>
           </Select>
         </div>
         <Snackbar
           anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
           open={this.state.open}
           autoHideDuration={2000}
-          message={<span id='message-id'>Format Changed To {format.toUpperCase()}!</span>}
+          message={
+            <span id="message-id">
+              Format Changed To {format.toUpperCase()}!
+            </span>
+          }
           ContentProps={{
             'aria-describedby': 'message-id'
           }}
           onClose={this.closeSnackbar}
           action={[
-            <IconButton onClick={this.closeSnackbar} color='inherit' key='close' aria-label='close'>
+            <IconButton
+              onClick={this.closeSnackbar}
+              color="inherit"
+              key="close"
+              aria-label="close"
+            >
               <CloseIcon />
             </IconButton>
           ]}
