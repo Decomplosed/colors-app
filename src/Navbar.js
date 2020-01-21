2a048794892f4476dbd662f835beff8dc14b11ae
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-21 22:28:31 +0100

    formatted the styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -10,6 +10,34 @@ import Slider from 'rc-slider'
 import 'rc-slider/assets/index.css'
 import './Navbar.css'
 
+const styles = {
+  Navbar: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+    height: '6vh',
+  },
+  logo: {
+    marginRight: '15px',
+    padding: '0 13px',
+    fontSize: '22px',
+    backgroundColor: '#eceff1',
+    fontFamily: 'Roboto',
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+  },
+  logo a: {
+    textDecoration: 'none',
+    color: '#000000'
+  },
+  slider: {
+    width: '340px',
+    margin: '0 10px',
+    display: 'inline-block'
+  }
+}
+
 export class Navbar extends Component {
   constructor(props) {
     super(props)
