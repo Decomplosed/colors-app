3d40eb0150a6408221a3e08113049f97e445dc63
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-21 22:40:07 +0100

    moved the Slider styles

--- src/Navbar.js
+++ src/Navbar.js
@@ -34,7 +34,23 @@ const styles = {
   slider: {
     width: '340px',
     margin: '0 10px',
-    display: 'inline-block'
+    display: 'inline-block',
+    '& .rc-slider-rail': {
+      height: '8px'
+    },
+    '& .rc-slider-handle, .rc-slider-handle:active, .rc-slider-handle:focus, .rc-slider-handle:hover': {
+      backgroundColor: 'green',
+      outline: 'none',
+      border: '2px solid green',
+      boxShadow: 'none',
+      width: '13px',
+      height: '13px',
+      marginLeft: '-7px',
+      marginTop: '-3px'
+    },
+    '& .rc-slider-track': {
+      backgroundColor: 'transparent'
+    }
   }
 }
 
