8fe1ee6f60a0e11c6eaf029c22fcbc227094b95f
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-21 22:00:39 +0100

    moved the footer styles to the component

--- src/PaletteFooter.js
+++ src/PaletteFooter.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
+const styles = {
+  PaletteFooter: {
+    backgroundColor: '#fff',
+    height: '5vh',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    fontWeight: 'bold'
+  },
+  Emoji: {
+    fontSize: '1.5rem',
+    margin: '1rem'
+  }
+}
+
 function PaletteFooter(props) {
-  const {paletteName, emoji} = props
+  const { paletteName, emoji } = props
   return (
-    <footer className='Palette-footer'>
+    <footer className="Palette-footer">
       {paletteName}
       <span className="emoji">{emoji}</span>
     </footer>
