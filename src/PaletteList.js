17a58a539d80c0c9b037c3b677242669674b385e
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-22 20:11:34 +0100

    import Link

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import MiniPalette from './MiniPalette'
 import { withStyles } from '@material-ui/styles'
 import styles from './styles/PaletteListStyles'
