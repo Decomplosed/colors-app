5077419ec21df07fa0da560bd9d1c27c594ac131
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-22 22:06:35 +0100

    added dummy colors to the state under colors array and print them out, created addNewColor method

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -77,7 +77,8 @@ class NewPaletteForm extends Component {
     super(props)
     this.state = {
       open: true,
-      currentColor: 'teal'
+      currentColor: 'teal',
+      colors: ['purple', '#312563']
     }
     this.updateCurrentColor = this.updateCurrentColor.bind(this)
   }
@@ -92,6 +93,10 @@ class NewPaletteForm extends Component {
 
   updateCurrentColor(newColor) {
     this.setState({ currentColor: newColor.hex })
+  }
+
+  addNewColor() {
+    this.setState({ colors: [...this.state.colors, this.state.currentColor] })
   }
 
   render() {
@@ -163,6 +168,11 @@ class NewPaletteForm extends Component {
           })}
         >
           <div className={classes.drawerHeader} />
+          <ul>
+            {this.state.colors.map(color => (
+              <li>{color}</li>
+            ))}
+          </ul>
         </main>
       </div>
     )
