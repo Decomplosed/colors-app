de8418bc8020cc9f35c3f3893991f65456b4777b
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-23 18:59:37 +0100

    created a constructor inside App.js and add savePalette as a props to the newPaletteForm

--- src/App.js
+++ src/App.js
@@ -9,14 +9,27 @@ import NewPaletteForm from './NewPaletteForm'
 import { generatePalette } from './colorHelpers'
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.savePalette = this.savePalette.bind(this)
+  }
+
   findPalette(id) {
     return seedColors.find(palette => palette.id === id)
+  }
+
+  savePalette(newPalette) {
+    console.log(newPalette)
   }
 
   render() {
     return (
       <Switch>
-        <Route exact path="/palette/new" render={() => <NewPaletteForm />} />
+        <Route
+          exact
+          path="/palette/new"
+          render={() => <NewPaletteForm savePalette={this.savePalette} />}
+        />
         <Route
           exact
           path="/"
