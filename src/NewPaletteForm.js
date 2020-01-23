eac6b1e7f5d1f3ad7ff2c4482d95e1159a9d1244
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-23 20:19:49 +0100

    created removeColor method

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -147,6 +147,12 @@ class NewPaletteForm extends Component {
     this.props.history.push('/')
   }
 
+  removeColor(colorName) {
+    this.setState({
+      colors: this.state.colors.filter(color => color.name !== colorName)
+    })
+  }
+
   render() {
     const { classes } = this.props
     const { open } = this.state
