d852a728e7e34ff2d09dfbdd1354ac0146d38d03
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-23 19:17:42 +0100

    savePalette

--- src/App.js
+++ src/App.js
@@ -13,13 +13,16 @@ class App extends Component {
     super(props)
     this.state = { palettes: seedColors }
     this.savePalette = this.savePalette.bind(this)
+    this.findPalette = this.findPalette.bind(this)
   }
 
   findPalette(id) {
     return this.state.palettes.find(palette => palette.id === id)
   }
 
-  savePalette(newPalette) {}
+  savePalette(newPalette) {
+    this.setState({ palettes: [...this.state.palettes, newPalette] })
+  }
 
   render() {
     return (
