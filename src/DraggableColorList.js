348b0af34cf440da76c37ee754501cad549fe7e1
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-24 13:29:49 +0100

    added missing arrow

--- src/DraggableColorList.js
+++ src/DraggableColorList.js
@@ -2,7 +2,7 @@ import React from 'react'
 import DraggableColorBox from './DraggableColorBox'
 import { SortableContainer } from 'react-sortable-hoc'
 
-const DraggableColorList = SortableContainer(({ colors, removeColor }) {
+const DraggableColorList = SortableContainer(({ colors, removeColor }) => {
   return (
     <div style={{ height: '100%' }}>
       {colors.map(color => (
