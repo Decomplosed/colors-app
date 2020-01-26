a28262359d995a3ed40a1a3a24a3a4a7754a03bf
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-26 21:48:44 +0100

    added more styles to the appBar

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -23,7 +23,9 @@ const styles = theme => ({
       duration: theme.transitions.duration.leavingScreen
     }),
     flexDirection: 'row',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    height: '64px'
   },
   appBarShift: {
     width: `calc(100% - ${drawerWidth}px)`,
