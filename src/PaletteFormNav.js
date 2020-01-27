aadb3c59b0a0cd00312a8e612557b8ce20ae110a
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-27 19:34:57 +0100

    extracted handleSubmit

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -57,7 +57,7 @@ class PaletteFormNav extends Component {
   }
 
   render() {
-    const { classes, open, palettes } = this.props
+    const { classes, open, palettes, handleSubmit } = this.props
     return (
       <div className={classes.root}>
         <CssBaseline />
