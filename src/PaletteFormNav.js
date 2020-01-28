c02d43417527364e82623e1c95e6f3e1b01fc671
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-28 16:48:24 +0100

    import styles

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -10,6 +10,7 @@ import Typography from '@material-ui/core/Typography'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
 import Button from '@material-ui/core/Button'
+import styles from './styles/PaletteFormNavStyles'
 
 class PaletteFormNav extends Component {
   constructor(props) {
