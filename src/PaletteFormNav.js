bece886bc33b194edc43101479bf50486f2c0c2c
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-28 15:33:40 +0100

    pass the method as a prop

--- src/PaletteFormNav.js
+++ src/PaletteFormNav.js
@@ -122,7 +122,11 @@ class PaletteFormNav extends Component {
           </div>
         </AppBar>
         {this.state.formShowing && (
-          <PaletteMetaForm palettes={palettes} handleSubmit={handleSubmit} />
+          <PaletteMetaForm
+            palettes={palettes}
+            handleSubmit={handleSubmit}
+            hideForm={this.hideForm}
+          />
         )}
       </div>
     )
