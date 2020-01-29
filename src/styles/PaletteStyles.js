d1f213559c4205c83ac41dd36c0dc81816ebc09b
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-29 21:03:47 +0100

    go back btn responsive in small devices

--- src/styles/PaletteStyles.js
+++ src/styles/PaletteStyles.js
@@ -37,6 +37,10 @@ export default {
       textTransform: 'uppercase',
       textDecoration: 'none',
       border: 'none'
+    },
+    [sizes.down('xs')]: {
+      width: '100%',
+      height: '10%'
     }
   }
 }
