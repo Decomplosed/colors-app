8e0f5b7fc7c9ae773ab3eac99042ea89e350e361
Bart Frackiewicz <decomplosed@gmail.com>
2020-01-29 19:36:58 +0100

    changed the component to be class and added constructor

--- src/MiniPalette.js
+++ src/MiniPalette.js
@@ -1,27 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { withStyles } from '@material-ui/styles'
 import styles from './styles/MiniPaletteStyles'
 import DeleteIcon from '@material-ui/icons/Delete'
+import { render } from '@testing-library/react'
 
-function MiniPalette(props) {
-  const { classes, paletteName, emoji, colors } = props
-  const miniColorBoxes = colors.map(color => (
-    <div
-      className={classes.miniColor}
-      style={{ backgroundColor: color.color }}
-      key={color.name}
-    />
-  ))
+class MiniPalette extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const { classes, paletteName, emoji, colors } = props
+    const miniColorBoxes = colors.map(color => (
+      <div
+        className={classes.miniColor}
+        style={{ backgroundColor: color.color }}
+        key={color.name}
+      />
+    ))
 
-  return (
-    <div className={classes.root} onClick={props.handleClick}>
-      <DeleteIcon className={classes.deleteIcon} onClick={this.deletePalette} />
-      <div className={classes.colors}>{miniColorBoxes}</div>
-      <h5 className={classes.title}>
-        {paletteName} <span className={classes.emoji}>{emoji}</span>
-      </h5>
-    </div>
-  )
+    return (
+      <div className={classes.root} onClick={props.handleClick}>
+        <DeleteIcon
+          className={classes.deleteIcon}
+          onClick={this.deletePalette}
+        />
+        <div className={classes.colors}>{miniColorBoxes}</div>
+        <h5 className={classes.title}>
+          {paletteName} <span className={classes.emoji}>{emoji}</span>
+        </h5>
+      </div>
+    )
+  }
 }
 
 export default withStyles(styles)(MiniPalette)
