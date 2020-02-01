e950297ce6daa829e8dbb8032c4ae77eb438cfc7
Bart Frackiewicz <decomplosed@gmail.com>
2020-02-01 13:50:32 +0100

    re-ordered imports

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { CSSTransition, TransitionGroup } from 'react-transition-group'
 import MiniPalette from './MiniPalette'
 import Dialog from '@material-ui/core/Dialog'
 import DialogTitle from '@material-ui/core/DialogTitle'
@@ -10,11 +11,10 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar'
 import ListItemText from '@material-ui/core/ListItemText'
 import CheckIcon from '@material-ui/icons/Check'
 import CloseIcon from '@material-ui/icons/Close'
-import { CSSTransition, TransitionGroup } from 'react-transition-group'
-import { withStyles } from '@material-ui/styles'
-import styles from './styles/PaletteListStyles'
 import blue from '@material-ui/core/colors/blue'
 import red from '@material-ui/core/colors/red'
+import { withStyles } from '@material-ui/styles'
+import styles from './styles/PaletteListStyles'
 
 export class PaletteList extends Component {
   constructor(props) {
